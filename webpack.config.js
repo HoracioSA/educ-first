2f1f64af7db9c881de4d09938ed004e9d8fb72e2
HoracioSA <hraciosapato2015hcs@gmail.com>
2021-02-19 19:59:19 +0200

    Edu first2

--- webpack.config.js
+++ webpack.config.js
@@ -61,7 +61,7 @@ module.exports={
     },
     output:{
         filename: 'bundle.js',
-        path: path.resolve(__dirname,'build'),
+        path: path.resolve(__dirname,'dist'),
         publicPath:"/"
     },
     plugins: [
